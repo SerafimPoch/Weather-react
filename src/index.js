62ee60e175b737d6cc74cefe89fc95426f5554df
SerafimPoch <serpochapinsky@gmail.com>
2018-12-26 20:38:42 +0200

    connect redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
+import reducer from "./store/reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
